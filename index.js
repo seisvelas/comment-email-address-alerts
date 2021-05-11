4dbac84e9382fb90b0455ffc435d8ff68db50106
Alex V <alej.alvarado94@gmail.com>
2021-05-11 16:25:34 -0600

    get correct comment

--- index.js
+++ index.js
@@ -2,14 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const secret = core.getInput('secret');
-  console.log(`Just seeing if this shows up!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const comment = payload.comment ? payload.comment.body : payload.issue.body;
+  console.log(`The comment: ${comment}`);
+  core.setOutput("emails", ""); // set emails here!
 } catch (error) {
   core.setFailed(error.message);
 }
