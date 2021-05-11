d9167d82679e14fbf9b3cd1cc6286878a1365943
Alex V <alej.alvarado94@gmail.com>
2021-05-11 17:03:43 -0600

    update

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ try {
   console.log(`comment: ${comment}`)
   const emails = comment.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)
   console.log(`emails: ${emails}`)
-  core.setOutput("emails", emails.join(", ")); // set emails here!
+  const prelude = "The following email addresses were found in the previous comment: ";
+  const epilogue = "\r\n\r\nPlease delete this comment from the issue once the addresses have been removed.";
+  core.setOutput(
+    "emails", 
+    emails 
+      ? prelude + emails.join(", ") + epilogue
+      : ""
+  );
 } catch (error) {
   core.setFailed(error.message);
 }
