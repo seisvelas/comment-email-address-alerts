df6e9e73c76013bc2d5dfa672ecb70e8d3a0b6af
Alex V <alej.alvarado94@gmail.com>
2021-05-11 16:49:28 -0600

    update

--- dist/index.js
+++ dist/index.js
@@ -6048,7 +6048,9 @@ const github = __nccwpck_require__(489);
 try {
   const payload = github.context.payload;
   const comment = payload.comment ? payload.comment.body : payload.issue.body;
+  console.log(`comment: ${comment}`)
   const emails = comment.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)
+  console.log(`emails: ${emails}`)
   core.setOutput("emails", emails.join(", ")); // set emails here!
 } catch (error) {
   core.setFailed(error.message);
--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const github = require('@actions/github');
 try {
   const payload = github.context.payload;
   const comment = payload.comment ? payload.comment.body : payload.issue.body;
+  console.log(`comment: ${comment}`)
   const emails = comment.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi)
+  console.log(`emails: ${emails}`)
   core.setOutput("emails", emails.join(", ")); // set emails here!
 } catch (error) {
   core.setFailed(error.message);
