3b33afaf62bfb41dcb4e28f3965eb836eeff8bf3
Alex V <alej.alvarado94@gmail.com>
2021-05-11 20:47:47 -0600

    update

--- dist/index.js
+++ dist/index.js
@@ -6066,7 +6066,7 @@ try {
     const options = {
         hostname: 'api.github.com',
         port: 443,
-        path: `/repos/${github.repository}/issues/${payload.issue.number}/comments`,
+        path: `/repos/${payload.issue.repository.full_name}/issues/${payload.issue.number}/comments`,
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
