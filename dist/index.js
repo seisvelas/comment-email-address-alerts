c6630e0b3db09af6d23130d52341692cd5446823
Alex V <alej.alvarado94@gmail.com>
2021-05-12 14:48:53 -0600

    update

--- dist/index.js
+++ dist/index.js
@@ -6065,11 +6065,11 @@ try {
     const repoToken = core.getInput('repo-token');
     const output = `
     There were ${emails.length} email addresses found in the above comment. Please:
-    
+
     1) click \`three dots\` -> \`edit\` to remove the email addresses
     2) click \`edited\` in the comment header, and click on the previous revision of the comment
     3) when viewing the old revision with an email in it, click \`options\` -> \`delete this revision from history\`
-    `.replace('    ', '');
+    `.replaceAll('    ', '');
 
     // make comment
     const data = JSON.stringify({
